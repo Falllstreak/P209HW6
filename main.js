ed1507b03ef996dcb0e8b9c38bfae70a340f57be
Falllstreak <23.anomaly@gmail.com>
2023-05-13 17:43:08 -0700

    Fixed Page Datarole issue with Hike Breakdown page. Tweaked List Hike page, formatted hike breakdown page. Added new parameters to Hike Objects.

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 let HikeArray = [];
 
-let HikeObject = function (pTrail, pWhere, pWhen, pTOD, pDistance, pNotes) {
+let HikeObject = function (pTrail, pWhere, pWhen, pTOD, pDistance, pElevation, pTime, pNotes) {
     this.trailname = pTrail;
     this.where = pWhere;
     this.when = pWhen;
     this.timeofday = pTOD;
     this.distance = pDistance;
+    this.elevation = pElevation;
+    this.time = pTime;
     this.note = pNotes;
 }
 
-HikeArray.push ( new HikeObject("Hike Mountain", "Washington", "2023-05-01", "Evening", "8 Miles", "This felt so much longer than it was.")  );
-HikeArray.push ( new HikeObject("Hike Hill", "Colorado", "2020-08-22", "Morning", "12 Miles", "I don't recommend mornings for this one.")  );
-HikeArray.push ( new HikeObject("Hike View", "Oregon", "2018-07-09", "Night", "3 Miles", "Easiest hike ever!")  );
+HikeArray.push ( new HikeObject("Hike Mountain", "Washington", "2023-05-01", "Evening", "8 Miles", "345 Feet", "3 hours", "This felt so much longer than it was.")  );
+HikeArray.push ( new HikeObject("Hike Hill", "Colorado", "2020-08-22", "Morning", "12 Miles", "570 Feet", "6 hours", "I don't recommend mornings for this one.")  );
+HikeArray.push ( new HikeObject("Hike View", "Oregon", "2018-07-09", "Night", "3 Miles", "200 Feet", "1 hours", "Easiest hike ever!")  );
 
 let selectedTOD = "";
 
@@ -24,26 +26,22 @@ document.addEventListener("DOMContentLoaded", function (event) {
     createList();
 
     document.getElementById("buttonAdd").addEventListener("click", function () {
-        HikeArray.push(new HikeObject(document.getElementById("trailInput").value, 
+        HikeArray.push(new HikeObject(
+        document.getElementById("trailInput").value, 
         document.getElementById("locationInput").value,
         document.getElementById("dateInput").value,
         selectedTOD,
         document.getElementById("distanceInput").value,
+        document.getElementById("trailElevation").value,
+        document.getElementById("trailTime").value,
         document.getElementById("noteInput").value,
         // movieArray.length,  // set ID
         ));
         document.location.href = "index.html#listHikes";
 
 
-
-        // HikeArray.push ( new HikeObject(document.getElementById("noteInput").value, selectedType,
-        // document.getElementById("trailInput").value ) );
-        // document.getElementById("locationInput").value = "";
-        // document.getElementById("dateInput").value = "";
-        // document.getElementById("noteInput").value = "";
-        // document.getElementById("trailInput").value = "";
-
         createList();
+        console.log(HikeArray);
     });
 
     $(document).bind("change", "#select-TOD", function (event, ui) {
@@ -67,7 +65,7 @@ function createList() {
     HikeArray.forEach(function (element,) {   // use handy array forEach method
         var li = document.createElement('li');
           // added data-role="listview" to the ul in the html
-        li.innerHTML = element.trailname + "    -    " + element.where + "    -    " + element.when + "    -    " + element.timeofday;
+        li.innerHTML = element.trailname + " -    ["  + element.when +  "]    ";
         myul.appendChild(li);
     });
 };
